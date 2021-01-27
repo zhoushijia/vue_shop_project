f755cf46960a6a54aa3f794cd3974a0eeeb253fb
zhoushijia <zhoushijia@foxmail.com>
2021-01-27 14:31:44 +0800

    项目初始清理

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 按需导入 element ui 组件
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
