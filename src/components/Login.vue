4d1bc4f3d3817c82904570604b654f2eb0de09d7
zhoushijia <zhoushijia@foxmail.com>
2021-01-27 16:57:22 +0800

    login页面的css样式

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="login_container">
-    <div class="login_box"></div>
+    <div class="login_box">
+      <div class="avatar">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <el-form class="form_box" label-width="0px">
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-3702mima"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button class="btn-1" type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -22,5 +38,38 @@ export default {}
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  .avatar {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 130px;
+    height: 130px;
+    background-color: #fff;
+    border-radius: 50%;
+    border: 1px solid #eee;
+    box-shadow: 0 0 10px #eee;
+    padding: 10px;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background: #eee;
+    }
+  }
+}
+.form_box {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0 20px;
+  .btns {
+    padding: 0 40px;
+    display: flex;
+    justify-content: space-between;
+    .btn-1 {
+      margin-right: 80px;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 // 按需导入 element ui 组件
 import './plugins/element.js'
+// 导入自定义全局样式
 import './assets/css/global.css'
+// 导入字体图标样式
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
