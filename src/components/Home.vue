57ad8afb9816ee6ecabeee27e3a8f4b126951b7b
zhoushijia <zhoushijia@foxmail.com>
2021-01-29 11:19:59 +0800

    退出功能实现

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>home</div>
+  <div>
+    home
+    <el-button type="info" @click="loginOut">退出</el-button>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    // 实现退出功能
+    loginOut() {
+      // 需要调用后端接口
+      // 清空token信息
+      window.sessionStorage.clear()
+      // 跳转到登录页
+      this.$router.push('/login')
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped></style>
