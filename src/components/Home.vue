ac00b8b1ef3ec17daf947e69a714c88cda58b482
zhoushijia <zhoushijia@foxmail.com>
2021-01-29 13:51:22 +0800

    页面搭建

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
-    home
-    <el-button type="info" @click="loginOut">退出</el-button>
-  </div>
+  <el-container class="home_container">
+    <el-header class="home_header">
+      <div>
+        <img src="../assets/top.png" alt="" />
+        <span>Tom电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="loginOut">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px" class="home_aside">Aside</el-aside>
+      <el-main class="home_main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -20,4 +29,32 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home_container {
+  height: 100%;
+}
+.home_header {
+  background-color: #373d41;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  > div {
+    display: flex;
+    align-items: center;
+    color: #fff;
+    img {
+      height: 100%;
+      border-radius: 50%;
+    }
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.home_aside {
+  background-color: #333744;
+}
+.home_main {
+  background-color: #eaedf1;
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
 
 // 挂载组件使用Vue.use()
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 
 // 挂载方法或者属性通过prototype
 // 挂载 弹框组件
